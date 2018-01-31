ff4a51b6699563bc177a3a5c5b324889688e99a9
davici <Davici@live.nl>
2018-01-31 14:37:48 +0100

    OPdracht 1-10

--- Weekofcode4/Program.cs
+++ Weekofcode4/Program.cs
@@ -15,13 +15,82 @@ namespace Weekofcode4
             Console.WriteLine(" Ik \n ben \n springerig"); 
         }
 
-        static void
+        static void Opdracht3()
+        {
+            Console.WriteLine(" Ik");
+            Console.WriteLine(" Doe");
+            Console.WriteLine(" Extra");
+            Console.WriteLine(" Werk");
+        }
+
+        static void Opdracht4()
+        {
+            Console.WriteLine(" Twee\n regels");
+        }
+
+        static void Opdracht5()
+        {
+            Console.WriteLine(" Tabbladen\tzijn\tgaaf");
+        }
+
+        static void Opdracht6()
+        {
+            Console.WriteLine("Kijk mama, zonder gegeven code!' afdrukt.");
+        }
+
+        static void Opdracht7()
+        {
+            Console.WriteLine( "Ik");
+            Console.Write("verzamel\n");
+            Console.WriteLine("vandaag");
+            Console.Write("de\n");
+            Console.WriteLine("meeste");
+            Console.Write("achievements\n");
+
+        }
+        static void Opdracht8()
+        {
+            Console.WriteLine("tab,\t" +
+                "tab,\t" +
+                " tab,\t" +
+                " die mooie tab van goud.");
+        }
+        static void Opdracht9()
+        {
+            Console.Write("Een\n");
+            Console.Write("Twee\n");
+            Console.Write("Drie\n");
+            Console.Write("Vier\n");
+            Console.Write("Vijf\n");
+            Console.Write("Zes\n");
+            Console.Write("Zeven\n");
+            Console.Write("Acht\n");
+            Console.Write("Negen\n");
+            Console.Write("Tien\n");
+        }
+        static void Opdracht10()
+        {
+            Console.WriteLine("Ik");
+            Console.WriteLine("hou");
+            Console.WriteLine("van");
+            Console.WriteLine("witregels");
+        }
+    
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+     
             Opdracht1();
             Opdracht2();
+            Opdracht3();
+            Opdracht4();
+            Opdracht5();
+            Opdracht6();
+            Opdracht7();
+            Opdracht8();
+            Opdracht9();
+            Opdracht10();
+
             Console.ReadKey();
         }
     }

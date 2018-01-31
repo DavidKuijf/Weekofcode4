53966be10187353459fece1ffcef73baf10b3654
davici <Davici@live.nl>
2018-01-31 14:04:48 +0100

    Project start

---
+++ Weekofcode4/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weekofcode4
+{
+    class Output
+    {
+        void Opdracht_1()
+        {
+            Console.WriteLine("Week of Code rocks!");
+        }
+    }
+}
---
+++ Weekofcode4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Weekofcode4
+{
+    class Program
+    {
+        static void Opdracht1()
+        {
+            Console.WriteLine("Week of Code rocks!");
+        }
+
+        static void Opdracht2()
+        {
+
+            Console.WriteLine(" Ik \n ben \n springerig"); 
+        }
+
+        static void
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Opdracht1();
+            Opdracht2();
+            Console.ReadKey();
+        }
+    }
+}
